5e054039c98b03d176f69c4d1d802540f5dfa032
Marco <i1820782@continental.edu.pe>
2021-01-18 13:40:16 -0500

    se programó la base de datos

---
+++ hotel/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from hotel.models import Type
+from hotel.models import Room
+
+# Register your models here.
+admin.site.register(Type)
+admin.site.register(Room)
---
+++ hotel/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from datetime import date
+
+# Create your models here.
+class Type(models.Model):
+    name = models.CharField(max_length = 100, verbose_name = 'Tipo de habitación')
+    created_on=models.DateTimeField(auto_now_add=True)
+    updated_on= models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['created_on']
+        verbose_name='Tipo'
+        verbose_name_plural='Tipos'
+
+    def __str__(self): 
+        return self.name
+
+class Room(models.Model):
+    number = models.CharField(max_length = 100, verbose_name = 'Número de habitación')
+    description = models.TextField(verbose_name = 'Descripción')
+    image = models.ImageField("Imagen", upload_to='hotel', blank=True)
+    created_on=models.DateTimeField(auto_now_add=True)
+    restaurant = models.ForeignKey('Type', on_delete=models.CASCADE)
+    updated_on= models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['created_on']
+        verbose_name='Habitación'
+        verbose_name_plural='Habitaciones'
+
+    def __str__(self): 
+        return 'R:{} -> T: {}'.format(self.number, self.type.name)
